2373695dd25e3b808f1f99c141ccbda28a8a9663
rl8384 <radoslaw.lis@dxc.com>
2022-11-24 22:05:35 +0100

    Update logging

--- cheater_app/logger.py
+++ cheater_app/logger.py
@@ -1,13 +1,23 @@
 import logging
 
-LOG_FILE = "debug.log"
-
-logging.basicConfig(format='%(asctime)s,%(msecs)d (%(filename)s, %(lineno)d) %(name)s %(levelname)s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=[
-                        logging.FileHandler(LOG_FILE, 'w+'),
-                        logging.StreamHandler()
-                    ],
-                    level=logging.INFO)
+DEBUG_LOG_FILE = "debug.log"
+INFO_LOG_FILE = "info.log"
 
 logger = logging.getLogger("root")
+
+
+formatter = logging.Formatter(
+    '%(asctime)s,%(msecs)d (%(filename)s, %(lineno)d) %(name)s %(levelname)s %(message)s')
+
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.FileHandler(INFO_LOG_FILE, 'w+')
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler(DEBUG_LOG_FILE, 'w+')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
